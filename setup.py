e148917b42787746d2a16ecb18fed4379dbbc759
Blake Rainwater <rainwaterblake@gmail.com>
2021-03-22 16:09:38 -0400

    Fix config parser import for python3

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ try:
   from StringIO import StringIO
 except ModuleNotFoundError:
   from io import StringIO
-from ConfigParser import ConfigParser
+try:
+  from ConfigParser import ConfigParser
+except ModuleNotFoundError:
+  from configparser import ConfigParser
 from setuptools import setup, find_packages
 
 # avoid sectionlesslessness (& case insensitivity) of ConfigParser

5e03b62a288a281fd962fb7b6cefbbbe7dcabe5f
Blake Rainwater <rainwaterblake@gmail.com>
2021-03-22 16:19:18 -0400

    Fix relative import for python3

--- pyfscache/__init__.py
+++ pyfscache/__init__.py
@@ -10,7 +10,7 @@ This forked version modified by Robert Clewley.
 from . _version import __version__
 
 from . fscache import *
-import tests
+from . import tests
 
 __all__ = ["FSCache", "make_digest",
            "auto_cache_function", "cache_function", "to_seconds"]
